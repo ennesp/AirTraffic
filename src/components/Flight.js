595b1819c3e8ef5583a300a0becb0c163920490b
Enes Plojovic <enes.plojovic1@gmail.com>
2018-08-07 01:55:17 +0200

    Flight details added

--- src/components/Flight.js
+++ src/components/Flight.js
@@ -1,19 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import logo from '../images/logo.png';
 import Header from './Header';
 
-const Flight = (props) => {
-    const flight = props.location.state.flight;
-    console.log(flight);
+const LOGO_FINDER_URL = 'https://autocomplete.clearbit.com/v1/companies/suggest?query=';
 
-    return (
-        <div className="App">
-            <Header />
+class Flight extends Component {
 
-            <main className="main-content">
-                <div>Flight Item</div>
-            </main>
-        </div>
-    );
+    constructor(props){
+        super(props);
+
+        this.state = {
+            logo: ''
+        }
+    }
+
+    getCompanyLogo = company => {
+        let img = logo;
+
+        axios(`${LOGO_FINDER_URL}${company}`).then(response => {
+            if(response.data.length > 0){
+                img = response.data[0].logo;
+                this.setState({logo: img})
+            }
+        });
+
+        return img;
+    }
+
+    componentDidMount = () => {
+        this.getCompanyLogo(this.props.location.state.flight.Op);
+    }
+
+    render(){
+        const flight = this.props.location.state.flight;
+        console.log(flight)
+        return (
+            <div className="App">
+                <Header />
+
+                <main className="main-content">
+                    <div>Flight Details</div>
+
+                    <div className="flight-details">
+                        <div className="info">
+                            {this.state.logo && <img className="company-logo" src={this.state.logo} alt={flight.Op} />}
+                        </div>
+
+                        <div className="info">
+                            <span>Airplane Manufacturer and model: </span>
+                            <span>{flight.Mdl}</span>
+                        </div>
+
+                        <div className="info">
+                            <span>From: </span>
+                            <span>{flight.From}</span>
+                        </div>
+
+                        <div className="info">
+                            <span>To: </span>
+                            <span>{flight.To}</span>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        );
+    }
 
 }
 
