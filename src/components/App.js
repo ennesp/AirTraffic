a1b48848dbed4026f172480fa61fc53a6ce3289a
Enes Plojovic <enes.plojovic1@gmail.com>
2018-08-06 17:51:05 +0200

    getting user location

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,71 @@ import Header from './Header';
 import List from './List'
 import '../App.css';
 
+const URL = 'https://cors-anywhere.herokuapp.com/https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lfDstU=120';
+
 class App extends Component {
-    render() {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            location: {},
+            error: ''
+        }
+
+        this.getFlights = this.getFlights.bind(this);
+    }
+
+    render(){
         return (
-        <div className="App">
-            <Header />
+            <div className="App">
+                <Header />
 
-            <main className="main-content">
-                <List />
-            </main>
+                <main className="main-content">
+                    <List />
+                </main>
 
-        </div>
+            </div>
         );
+    }
+
+    componentDidMount(){
+        //Getting user location
+        if (navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(this.getFlights, this.showError);
+        }
+    }
+
+    getFlights(position){
+        this.setState({
+            location: {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            }
+        });
+        console.log(this.state.location)
+    }
+
+    showError(error) {
+        let err;
+        switch(error.code) {
+            case error.PERMISSION_DENIED:
+                err = "User denied the request for Geolocation."
+                break;
+            case error.POSITION_UNAVAILABLE:
+                err = "Location information is unavailable."
+                break;
+            case error.TIMEOUT:
+                err = "The request to get user location timed out."
+                break;
+            case error.UNKNOWN_ERROR:
+                err = "An unknown error occurred."
+                break;
+        }
+
+        this.setState({
+            error: err
+        });
     }
 }
 
